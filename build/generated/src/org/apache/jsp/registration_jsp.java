b01ae826d4224254997e4738eb6af15b7cb05e95
inshu27 <inshu.malhotra@gmail.com>
2017-01-22 16:13:40 +0530

     

---
+++ build/generated/src/org/apache/jsp/registration_jsp.java
@@ -0,0 +1,83 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class registration_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"bootstrap.min.css\"/>\n");
+      out.write("        <script src=\"jquery.min.js\" > </script>\n");
+      out.write("        <script src=\"js_bootstrap.min.css\" > </script> \n");
+      out.write("        <link href=\"https://fonts.googleapis.com/css?family=Oswald|Patua+One\" rel=\"stylesheet\">\n");
+      out.write("        <!-- Bootstrap theme -->\n");
+      out.write("    <link href=\"css/bootstrap-theme.min.css\" rel=\"stylesheet\">\n");
+      out.write("    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->\n");
+      out.write("    <link href=\"css/ie10-viewport-bug-workaround.css\" rel=\"stylesheet\">\n");
+      out.write("    <script src=\"js/ie-emulation-modes-warning.js\"></script>\n");
+      out.write("        <title>JSP Page</title>\n");
+      out.write("        <style>\n");
+      out.write("            h1{\n");
+      out.write("                font-family: 'Patua One', cursive;\n");
+      out.write("            }\n");
+      out.write("        </style>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <h1>Hello World!</h1>\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ build/web/registration.jsp
@@ -0,0 +1,96 @@
+<%-- 
+    Document   : registration
+    Created on : Jan 22, 2017, 2:39:46 PM
+    Author     : Inshu
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="bootstrap.min.css"/>
+        <script src="jquery.min.js" > </script>
+        <script src="js_bootstrap.min.css" > </script> 
+        <link href="https://fonts.googleapis.com/css?family=Lato|Oswald|Patua+One" rel="stylesheet">
+        <!-- Bootstrap theme -->
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <script src="js/ie-emulation-modes-warning.js"></script>
+        <title>JSP Page</title>
+        <style> body{
+                background-image: url("https://digitizemeblog.files.wordpress.com/2011/08/background-gradient-2.jpg");
+                background-position-x: 50%;
+            }
+            h1{
+                font-family: 'Patua One', cursive;
+            }
+            .container{
+                background-color: #f6f9c7;
+                height: 75vh;
+                width: 40%;
+                border-radius: 1%;
+                position: relative;
+                margin-top: 12.5vh;
+                border: 4px solid #2979a9;
+                padding: 0;
+            }
+            .heading{
+                width: 100%;
+                background-color: #d98549;
+                padding: 2%;
+                
+                margin: 0 0 6vh 0;
+                text-align: center;
+                border: 1px solid #2979a9;
+            }
+            .heading>h1{ margin: 0; }
+            label{
+                color: #123456;
+                font-family: lato;
+                font-size: 1.2em;
+            }
+            input{
+                font-family: lato;
+                font-weight: bold;
+                font-size: 1.5em;
+                border-radius: 50%;
+                
+            }
+            form{
+                width: 85%;
+                padding: 10%;
+                //background-color: #a8a8a8;
+                margin: 0 auto;
+            }
+            @media screen and (max-width: 860px) {
+                .container {
+                    width: 100%;
+                    margin-top: 8.5vh;
+                }
+}
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="heading">
+                <h1>TEST YOUR SKILLS</h1>
+            </div>
+            <form>
+                <div class="form-group">
+                  <label for="email">Email address:</label>
+                  <input style="border-radius: 20px;" type="email" class="form-control" id="email">
+                </div>
+                <div class="form-group">
+                  <label for="pwd">Password:</label>
+                  <input style="border-radius: 20px;"  type="password" class="form-control" id="pwd">
+                </div>
+                <div class="checkbox">
+                  <label><input type="checkbox"> Remember me</label>
+                </div>
+                <button style="border-radius: 30px; width: 8em;" type="submit" class="btn btn-default">Register</button>
+              </form>
+        </div>
+    </body>
+</html>
